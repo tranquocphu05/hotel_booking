53e5d7865fbf23aa39ff3687872b806c8b0599fd
Trần Quốc Phú <tranphu16172005@gmail.com>
2025-10-05 20:51:59 +0700

    update migration files

---
+++ database/migrations/2025_10_05_134829_create_danh_gia_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('danh_gia', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('danh_gia');
+    }
+};
