c6ad6b83ca46b5ebbf4603065c193360315f96a6
dattrn09 <kineer098@gmail.com>
2025-11-14 10:10:26 +0700

    Fix: Add VNPay configuration to services.php

--- config/services.php
+++ config/services.php
@@ -41,4 +41,10 @@ return [
         'redirect' => env('GOOGLE_REDIRECT_URI', 'http://127.0.0.1:8000/auth/google/callback'),
     ],
 
+    'vnpay' => [
+        'tmn_code' => env('VNPAY_TMN_CODE'),
+        'hash_secret' => env('VNPAY_HASH_SECRET'),
+        'url' => env('VNPAY_URL', 'https://sandbox.vnpayment.vn/paymentv2/vpcpay.html'),
+    ],
+
 ];
