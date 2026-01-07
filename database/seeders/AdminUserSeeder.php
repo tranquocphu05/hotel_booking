bfdd572debeeb55761ff5bb1406371414808fd56
Trần Quốc Phú <tranphu16172005@gmail.com>
2026-01-08 01:24:42 +0700

    phu code

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -7,7 +7,6 @@ use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
-
 class AdminUserSeeder extends Seeder
 {
     /**
